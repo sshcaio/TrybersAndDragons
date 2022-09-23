62317cffd5d156b341497d874cae6354b896664b
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-09-23 00:04:57 -0300

    [feat] Elf class extended from Race
    New race class extended from the abstract

---
+++ src/Races/Elf.ts
@@ -0,0 +1,28 @@
+import Race from './Race';
+
+class Elf extends Race {
+  private static _raceInstance = 0;
+
+  constructor(
+    _name: string,
+    _dexterity: number,
+    private _maxLifePoints = 99,
+  ) {
+    super(
+      _name,
+      _dexterity,
+    )
+  }
+
+  public get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  public static createdRacesInstances(): number {
+    this._raceInstance++;
+    return this._raceInstance;
+  }
+
+}
+
+export default Elf;
