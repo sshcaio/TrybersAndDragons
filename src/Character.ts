7433e1c7458eb7ad2d9e52ee6ffae029c1a249a0
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-09-23 06:37:48 -0300

    [lint]

--- src/Character.ts
+++ src/Character.ts
@@ -19,12 +19,15 @@ export default class Character implements Fighter {
     this._name = name;
     this._dexterity = getRandomInt(1, 10);
     this._strength = getRandomInt(1, 10);
-    this._defense  = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
     this._race = new Elf(this._name, this._dexterity);
     this._archetype = new Mage(this._name);
     this._maxLifePoints = this._race.maxLifePoints / 2;
     this._lifePoints = this._maxLifePoints;
-    this._energy = { type_: this._archetype.energyType, amount: getRandomInt(1, 10) }
+    this._energy = {
+      type_: this._archetype.energyType, 
+      amount: getRandomInt(1, 10),
+    };
   }
 
   public get name(): string {
@@ -93,10 +96,10 @@ export default class Character implements Fighter {
     const criticalHit = (strengthTest / 3) <= rollTheDice;
 
     if (criticalHit) {
-      enemy.lifePoints -= this._strength * 2;
+      enemy.receiveDamage(this._strength * 2);
       return;
     }
 
-    enemy.lifePoints -= this._strength * 1.2;
+    enemy.receiveDamage(this._strength * 1.2);
   }
 }
