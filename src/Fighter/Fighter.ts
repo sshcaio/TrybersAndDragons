d0b8425130c3c8a250b9d23366171e5864899a35
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-09-23 08:16:06 -0300

    [feat] SimpleFighter interface

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -6,8 +6,8 @@ interface Fighter {
   defense: number,
   energy?: Energy
 
-  attack?(enemy: Fighter): void
-  special(enemy: Fighter): void
+  attack(enemy: Fighter): void
+  special?(enemy: Fighter): void
   levelUp(): void
   receiveDamage(attackPoints: number): number
 }
